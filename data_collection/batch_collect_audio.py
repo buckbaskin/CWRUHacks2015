94b3d5b68e6888facb3e310aa5850afd9cd23b42
buckbaskin <mobile.wbaskin@gmail.com>
2015-02-07 07:00:15 -0500

    Work on collecting files.

--- data_collection/batch_collect_audio.py
+++ data_collection/batch_collect_audio.py
@@ -1,1 +1,28 @@
 __author__ = 'buckbaskin'
+
+import os
+from shutil import copytree
+
+start_folder = r'C:\Users\mlh\Music\iTunes\iTunes Media\Music'
+destination_folder = r'C:\Users\mlh\Documents\GitHub\CWRUHacks2015\Assets\sounds'
+
+def short_dir(full_name):
+    for i in range(len(full_name)-1, 0, -1):
+        if full_name[i:i+1] == "\\":
+            return full_name[i:]
+
+def main():
+    count = 0
+    print 'count'
+    for dirName, subdirList, fileList in os.walk(start_folder):
+        print('Processing directory: %s' % dirName)
+        for fname in fileList:
+            if fname[-4:] == '.mp3':
+                print('\tmusic copied from dir %s to location %s' % (dirName , destination_folder+short_dir(dirName)))
+                count += len(fileList)
+                copytree(dirName , destination_folder+short_dir(dirName))
+                break
+    print 'count ' + str(count)
+
+if __name__ == '__main__':
+    main()
--- data_collection/batch_collect_images.py
+++ data_collection/batch_collect_images.py
@@ -1,2 +1,22 @@
 __author__ = 'buckbaskin'
 
+import os
+from shutil import copy
+
+start_folder = r'C:\Users\mlh\Downloads'
+destination_folder = r'C:\Users\mlh\Documents\GitHub\CWRUHacks2015\Assets\images'
+
+def main():
+    print 'main'
+    count = 0
+    for dirName, subdirList, fileList in os.walk(start_folder):
+        print('Processing directory: %s' % dirName)
+        for fname in fileList:
+            if fname[-4:] == '.jpg': # or fname[-4:] == '.png':
+                print('\timage copied from file %s to location %s' % (fname , destination_folder))
+                count += 1
+                copy(start_folder+fname , destination_folder)
+        break
+
+if __name__ == '__main__':
+    main()
